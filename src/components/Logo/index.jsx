caf7a4d00ed8b60cba11340f1c2be0c811e396d8
mtssantos96 <matheus.p.1996@hotmail.com>
2023-07-11 20:06:15 -0300

    feat: Atualizando o logo do projeto

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import { BiMusic } from 'react-icons/bi';
+import { BiHeadphone } from 'react-icons/bi';
 import styles from './style.module.css';
 
 class Logo extends Component {
   render() {
     return (
       <div className={ styles.logoContainer }>
-        <BiMusic />
-        <h1>TrybeTunes</h1>
+        <div>
+          <h1>trybe</h1>
+          <BiHeadphone />
+        </div>
+        <p>tunes</p>
       </div>
     );
   }
